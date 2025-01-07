6ed1096e624c2dc7349a312ab1811f8a7dd2dd34
Rachel Opperman <rachel@zaengle.com>
2025-01-07 11:08:29 -0500

    Removes TODO

--- src/web/assets/neverstale/src/types/FetchApiContentResponse.ts
+++ src/web/assets/neverstale/src/types/FetchApiContentResponse.ts
@@ -1,7 +1,6 @@
 import { ContentFlag } from './ContentFlag.ts'
 
 export interface FetchApiContentResponse {
-  // TODO: Make this an enum
   analysis_status: string
   analyzed_at: string
   custom_id: string
